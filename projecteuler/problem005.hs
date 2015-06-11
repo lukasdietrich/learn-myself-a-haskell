38d734b3d7aad25178e4aecd95b7c5d3b71189ac
Lukas Dietrich <lukas@lukasdietrich.com>
2015-06-11 21:41:48 +0200

    enhanced problem #5

--- projecteuler/problem005.hs
+++ projecteuler/problem005.hs
@@ -2,10 +2,14 @@ isEvenlyDivisable :: Int -> [Int] -> Bool
 isEvenlyDivisable _ []      = True
 isEvenlyDivisable i (x:xs)  = mod i x == 0 && isEvenlyDivisable i xs
 
+filterDivisor :: [Int] -> [Int]
+filterDivisor []     = []
+filterDivisor (x:xs) = x:(filterDivisor [y | y <- xs, mod x y /= 0])
+
 findDivisable :: [Int] -> [Int] -> Int
 findDivisable (x:xs) d
     | isEvenlyDivisable x d = x
     | otherwise             = findDivisable xs d
 
 main :: IO ()
-main = print $ findDivisable [20,40..] [1..20]
+main = print $ findDivisable [20,40..] $ filterDivisor [20,19..1]
