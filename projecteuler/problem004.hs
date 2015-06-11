e73d5bba3138b23d8f774ee473ae31b384be0fbf
Lukas Dietrich <lukas@lukasdietrich.com>
2015-06-11 23:12:59 +0200

    enhanced problem #4 performance

--- projecteuler/problem004.hs
+++ projecteuler/problem004.hs
@@ -3,8 +3,7 @@ isPalindrome s = s == reverse s
 
 findPalindrome :: [(Int, Int, Int)]
 findPalindrome = 
-    [(x, y, x * y) | x <- range, y <- range, isPalindrome $ show $ x * y]
-    where range = [100..999]
+    [(x, y, x * y) | x <- [100..999], y <- [x..999], isPalindrome $ show $ x * y]
 
 betterPalindrome :: (Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
 betterPalindrome a@(_, _, b) c@(_, _, d)
